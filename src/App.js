334c7e912245371eae31e73371ad454ce22c12a7
Andrew Tjong <72036357+andrewtjong@users.noreply.github.com>
2022-12-30 16:52:26 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+
+// page components
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <nav>
+        <h1>My Articles</h1>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/contact"> 
+        <Contact /> 
+        </Route>
+      </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h2>Homepage</h2>
+            <p>Lorem ipsum hocus pocus</p>
+        </div>
+    )
+}
