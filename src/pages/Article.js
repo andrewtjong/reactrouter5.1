30b55746ae2522219afd51147bede8df366e4a86
Andrew Tjong <72036357+andrewtjong@users.noreply.github.com>
2023-01-04 14:07:11 +0100

    add(import article data and display)

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom"
+import useFetch from "../hooks/useFetch"
 
 export default function Article() {
     // returns an object with params that has all the route parameters from this page, so it will look at this page and see we have an id parameter
@@ -7,12 +8,23 @@ export default function Article() {
     // const params = useParams()
 
     // use object destructuring to turn it into
-
     const { id } = useParams()
+    const url = 'http://localhost:3000/articles/' + id
+    // call that data article instead
+    const {data: article, isPending, error} = useFetch(url)
 
     return(
         <div>
-            <p>Article Page - {id}</p>
+            {isPending && <div>Loading....</div>}
+            {error && <div>{error}</div>}
+            {article && (
+                <div> 
+                    <h2>{article.title}</h2>
+                    <p>{article.author}</p>
+                    <p>{article.body}</p>
+
+                </div>
+            )}
         </div>
     )
 }
